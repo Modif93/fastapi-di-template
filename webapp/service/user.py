34ae55c64884ce70af06e487eef581c9df31439a
Modif93 <57270858+Modif93@users.noreply.github.com>
2023-11-12 22:31:21 +0900

    Update user with abstract class

--- webapp/service/user.py
+++ webapp/service/user.py
@@ -1,17 +1,18 @@
 """Services module."""
 
 from uuid import uuid4
-from typing import Iterator
+from typing import Annotated, Iterator
+
+from ..core.abstract import Service
 
 from ..repository.user import UserRepository
 from ..model.user import User
 
 
-class UserService:
-
-    def __init__(self, user_repository: UserRepository) -> None:
-        self._repository: UserRepository = user_repository
-
+class UserService(Service):
+    _repository: Annotated[UserRepository,"user_repository"]
+    
+    
     def get_users(self) -> Iterator[User]:
         return self._repository.get_all()
 
