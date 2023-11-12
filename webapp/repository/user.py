70b3900ecf0d89a055e1277da267ccb996738629
Modif93 <57270858+Modif93@users.noreply.github.com>
2023-11-12 23:16:09 +0900

    Update user with abstract class

--- webapp/repository/user.py
+++ webapp/repository/user.py
@@ -5,13 +5,12 @@ from typing import Callable, Iterator
 
 from sqlalchemy.orm import Session
 
+from ..core.abstract import Repository
+
 from ..model.user import User
 
 
-class UserRepository:
-
-    def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]]) -> None:
-        self.session_factory = session_factory
+class UserRepository(Repository):
 
     def get_all(self) -> Iterator[User]:
         with self.session_factory() as session:
