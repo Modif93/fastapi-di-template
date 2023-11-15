f3716dc8110d9828ff3d76f3a38fba6408c9d131
Modif93 <57270858+Modif93@users.noreply.github.com>
2023-11-15 19:56:56 +0900

    Update user.py

--- webapp/repository/user.py
+++ webapp/repository/user.py
@@ -6,7 +6,7 @@ from typing import Callable, Iterator
 from sqlalchemy.orm import Session
 
 from ..core.abstract import Repository
-
+from ..exception.entity import NotFoundError
 from ..model.user import User
 
 
@@ -16,11 +16,11 @@ class UserRepository(Repository):
         with self.session_factory() as session:
             return session.query(User).all()
 
-    def get_by_id(self, user_id: int) -> User:
+    def get_by_id(self, _id: int) -> User:
         with self.session_factory() as session:
-            user = session.query(User).filter(User.id == user_id).first()
+            user = session.query(User).filter_by(id=_id).first()
             if not user:
-                raise UserNotFoundError(user_id)
+                raise UserNotFoundError(_id)
             return user
 
     def add(self, email: str, password: str, is_active: bool = True) -> User:
@@ -31,22 +31,13 @@ class UserRepository(Repository):
             session.refresh(user)
             return user
 
-    def delete_by_id(self, user_id: int) -> None:
+    def delete_by_id(self, _id: int) -> None:
         with self.session_factory() as session:
-            entity: User = session.query(User).filter(User.id == user_id).first()
+            entity: User = session.query(User).filter_by(id=_id).first()
             if not entity:
-                raise UserNotFoundError(user_id)
+                raise UserNotFoundError(_id)
             session.delete(entity)
             session.commit()
-
-
-class NotFoundError(Exception):
-
-    entity_name: str
-
-    def __init__(self, entity_id):
-        super().__init__(f"{self.entity_name} not found, id: {entity_id}")
-
 
 class UserNotFoundError(NotFoundError):
 
